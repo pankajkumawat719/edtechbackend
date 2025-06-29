4a52f5caaca032d7f7c3f43cb367a547fc823d0e
pankajkumawat719 <kumawatpankaj719@gmail.com>
2025-06-30 02:46:32 +0530

    database.js file  added

--- config/database.js
+++ config/database.js
@@ -1,1 +1,21 @@
-  
+// Database configuration file for a Node.js application using Mongoose
+const mongoose = require("mongoose");
+
+// Load environment variables from .env file
+require("dotenv").config();
+
+exports.connect = () => {
+  mongoose
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB successfully");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+      console.log(err);
+      process.exit(1); // Exit the process with failure
+    });
+};
